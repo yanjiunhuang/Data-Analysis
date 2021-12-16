a1321f4b0ab652c8b77beb0daf4e6816bbb7bf11
unknown <tham.tpn.2011@gmail.com>
2021-12-16 13:07:31 +0800

    Script

--- Data/Group_Script.R
+++ Data/Group_Script.R
@@ -4,9 +4,11 @@ DF <- read_excel("data/GroupAssignmentData.xls")
 readxl::excel_sheets("data/GroupAssignmentData.xls")
 Metadata <-read_excel("data/GroupAssignmentData.xls",sheet = "Metadata")
 DF$Field_Mgmt <- as.numeric(factor(DF$Field_Mgmt))
+DF$Treatment <- as.numeric(factor(DF$Treatment))
 
-M1 <- lm(aphid_live~Field_Mgmt+Pt.seminatural+Date+Treatment_ID+I(Field_Mgmt*Date),
-         data=DF)
+M1 <- lm(aphid_live~Field_Mgmt+Pt.seminatural+Date+Treatment_ID+I(Field_Mgmt*Date)
+         +I(Pt.seminatural*Date)+I(Treatment*Date)+I(Treatment*Pt.seminatural)
+         +I(Pt.seminatural*Treatment*Date),data=DF)
 summary(M1)
 anova(M1)
 AIC(M1)
